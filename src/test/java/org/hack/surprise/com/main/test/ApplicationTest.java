74aebbc0cfdccc7e44ba870b2f953c8a37518b66
Sai0100 <47456242+Sai0100@users.noreply.github.com>
2019-02-21 17:27:12 +0530

    Update ApplicationTest.java

--- src/test/java/org/hack/surprise/com/main/test/ApplicationTest.java
+++ src/test/java/org/hack/surprise/com/main/test/ApplicationTest.java
@@ -14,6 +14,9 @@ public class ApplicationTest {
 	@BeforeClass
 	public static void initData() {
 		System.out.println("Init data");
+		System.setProperty("webdriver.chrome.driver", "Path of Chrome driver a exe file"); 
+		WebDriver driver = new ChromeDriver();
+		driver.get("URL");
 	}
 	
 	
@@ -21,13 +24,37 @@ public class ApplicationTest {
 	@Test
 	public void runTest() {
 		System.out.println("Sample test");
+		}
+	
+	public void WannaHelp(){
+	 WebElement From = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-valid ui-autocomplete-input ng-empty ng-touched']"));
+	From.SendKeys("Hyderabad");	
+	 WebElement To = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-untouched ng-valid ui-autocomplete-input ng-empty']"));
+	To.SendKeys("Pocharam");				       
+	WebElement NeedHelp = driver.findElement(By.xpath("//button[contains(text(), 'Need' )]"));
+	NeedHelp.click();
+	WebElement validate = driver.findElement(By.xpath("//div[@class = 'form-inline']/h4"));
+	String validationText = validate.gettext();
+		
+	assertEquals("Would like to send Gift/Parcel ?    ", validationText);
 	}
 	
-	
-
-	
+	public void NeedHelp()
+	{
+	 WebElement From = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-valid ui-autocomplete-input ng-empty ng-touched']"));
+	From.SendKeys("Pocharam");	
+	 WebElement To = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-untouched ng-valid ui-autocomplete-input ng-empty']"));
+	 To.SendKeys("Hyderabad");						       	
+	WebElement WannaHelp = driver.findElement(By.xpath(""//button[contains(text(), 'Wanna' )]"));
+	WannaHelp.click();
+	WebElement validate = driver.findElement(By.xpath("//div[@class = 'form-inline']/h4"));
+	String validationText = validate.gettext();
+	assertEquals("Would like to help others by carrying a Gift/Parcel ?
+						   ", validationText);
+							   }
 	@AfterClass
 	public static void terminateTests() {
 		System.out.println("Terminate");
+		driver.quit();
 	}
 }
