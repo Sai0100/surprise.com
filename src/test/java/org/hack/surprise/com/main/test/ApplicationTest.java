f378f9e3417d94350b597b9afdce206e53481ab2
Saikumar Karnate <Saikumar Karnate@Saikumar Karnate-PC>
2019-02-21 23:33:07 +0530

    added test cases for wanna help and need help back and adding detail

--- src/test/java/org/hack/surprise/com/main/test/ApplicationTest.java
+++ src/test/java/org/hack/surprise/com/main/test/ApplicationTest.java
@@ -1,57 +1,71 @@
 package org.hack.surprise.com.main.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.hack.surprise.com.main.Application;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ApplicationTest {
-	
+
+	private static WebDriver driver;
+
 	@BeforeClass
 	public static void initData() {
-		System.out.println("Init data");
-		System.setProperty("webdriver.chrome.driver", "Path of Chrome driver a exe file"); 
-		WebDriver driver = new ChromeDriver();
-		driver.get("URL");
+		System.setProperty("webdriver.chrome.driver", "E:\\Sai\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://localhost:9092/#!/home");
 	}
-	
-	
+
+	@Test
+	public void ad_needHelp() throws InterruptedException { 
+		WebElement From = driver.findElement(By.xpath("//input[@id = 'from']"));
+		From.sendKeys("Pocharam");
+		WebElement To = driver.findElement(By.xpath("//input[@id = 'to']"));
+		To.sendKeys("Hyderabad");
+		WebElement WannaHelp = driver.findElement(By.xpath("//button[@id = 'need_id']"));
+		WannaHelp.click();
+		Thread.sleep(3000); 
+	}
 	
 	@Test
-	public void runTest() {
-		System.out.println("Sample test");
-		}
-	
-	public void WannaHelp(){
-	 WebElement From = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-valid ui-autocomplete-input ng-empty ng-touched']"));
-	From.SendKeys("Hyderabad");	
-	 WebElement To = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-untouched ng-valid ui-autocomplete-input ng-empty']"));
-	To.SendKeys("Pocharam");				       
-	WebElement NeedHelp = driver.findElement(By.xpath("//button[contains(text(), 'Need' )]"));
-	NeedHelp.click();
-	WebElement validate = driver.findElement(By.xpath("//div[@class = 'form-inline']/h4"));
-	String validationText = validate.gettext();
-		
-	assertEquals("Would like to send Gift/Parcel ?    ", validationText);
+	public void ac_back() throws InterruptedException {
+		driver.navigate().back();
+		Thread.sleep(3000); 
 	}
+
 	
-	public void NeedHelp()
-	{
-	 WebElement From = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-valid ui-autocomplete-input ng-empty ng-touched']"));
-	From.SendKeys("Pocharam");	
-	 WebElement To = driver.findElement(By.xpath("//input[@class = 'form form-control input-md ng-pristine ng-untouched ng-valid ui-autocomplete-input ng-empty']"));
-	 To.SendKeys("Hyderabad");						       	
-	WebElement WannaHelp = driver.findElement(By.xpath(""//button[contains(text(), 'Wanna' )]"));
-	WannaHelp.click();
-	WebElement validate = driver.findElement(By.xpath("//div[@class = 'form-inline']/h4"));
-	String validationText = validate.gettext();
-	assertEquals("Would like to help others by carrying a Gift/Parcel ?
-						   ", validationText);
-							   }
+	@Test
+	public void ab_addFromTo() throws InterruptedException {
+		Thread.sleep(3000); 
+		WebElement WannaHelp = driver.findElement(By.xpath(".//*[@id = 'accept']"));
+		WannaHelp.click();
+		Thread.sleep(3000); 
+	}
+
+	@Test
+	public void aa_wannaHelp() {
+		WebElement From = driver.findElement(By.xpath("//input[@id = 'from']"));
+		From.sendKeys("Pocharam");
+		WebElement To = driver.findElement(By.xpath("//input[@id = 'to']"));
+		To.sendKeys("Hyderabad");
+		WebElement WannaHelp = driver.findElement(By.xpath("//button[@id = 'wanna_id']"));
+		WannaHelp.click();
+	}
+
 	@AfterClass
 	public static void terminateTests() {
 		System.out.println("Terminate");
