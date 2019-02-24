e99a1aedf65c282313a78a72998cd10dc2e8d01a
Sai0100 <47456242+Sai0100@users.noreply.github.com>
2019-02-24 15:54:22 +0530

    Update ApplicationTest.java

--- src/test/java/org/hack/surprise/com/main/test/ApplicationTest.java
+++ src/test/java/org/hack/surprise/com/main/test/ApplicationTest.java
@@ -1,39 +1,56 @@
 package org.hack.surprise.com.main.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.hack.surprise.com.main.Application;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ApplicationTest {
-	
-	@Autowired
-	private MockMvc mockMvc;
-	
+
+	//A&e5Wxs7NpI8dvRkGlOeTcMI-mVt?54B
+
 	@BeforeClass
 	public static void initData() {
-		System.out.println("Init data");
+		System.out.println("Init");
 	}
-	
-	
+
+	@Test
+	public void ad_needHelp() throws InterruptedException { 
+		System.out.println("NeedHelp");
+	}
 	
 	@Test
-	public void runTest() {
-		System.out.println("Sample test");
+	public void ac_back() throws InterruptedException {
+		System.out.println("Back");
 	}
-	
-	
 
 	
+	@Test
+	public void ab_addFromTo() throws InterruptedException {
+		System.out.println("AddFromTo");
+	}
+
+	@Test
+	public void aa_wannaHelp() {
+		System.out.println("Wanna help");
+	}
+
 	@AfterClass
 	public static void terminateTests() {
-		System.out.println("Terminate");
+		System.out.println("Exit");
 	}
 }
-
